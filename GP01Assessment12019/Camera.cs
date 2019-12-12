423a14ee2afd022149a9b766354c9cd455bc1c47
CianO'Reilly <46957507+GitSmugg@users.noreply.github.com>
2019-12-12 15:40:43 +0000

    cameracode

--- GP01Assessment12019/Camera.cs
+++ GP01Assessment12019/Camera.cs
@@ -1,30 +1,53 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace GP01Assessment12019
+namespace CameraNS
 {
-    public class Camera
+    class Camera
     {
-        public Matrix Transform { get; private set; }
+        Vector2 _camPos = Vector2.Zero;
+        Vector2 _worldBound;
+        public Matrix CurrentCameraTranslation { get
+            {
+                return Matrix.CreateTranslation(new Vector3(
+                    -CamPos,
+                    0));
+            } }
 
-        public void Follow(Sprite target)
+        public Vector2 CamPos
         {
-            var position = Matrix.CreateTranslation(
-                -target.Position.X - (target.Rectangle.Width / 2),
-                -target.Position.Y - (target.Rectangle.Height / 2),
-                0);
-                
-                
-                var offset = Matrix.CreateTranslation(
-                    Game1.ScreenWidth / 2,
-                    Game1.ScreenHeight / 2,
-                    0);
+            get
+            {
+                return _camPos;
+            }
 
-            Transform = position * offset;
+            set
+            {
+                _camPos = value;
+            }
         }
+
+        public Camera(Vector2 startPos, Vector2 bound)
+        {
+            CamPos = startPos;
+            _worldBound = bound;
+        }
+
+        public void move(Vector2 delta, Viewport v)
+        {
+            CamPos += delta;
+            CamPos = Vector2.Clamp(CamPos, Vector2.Zero, _worldBound - new Vector2(v.Width, v.Height));
+        }
+
+        public void follow(Vector2 followPos, Viewport v)
+        {
+            _camPos = followPos - new Vector2(v.Width / 2, v.Height / 2);
+            _camPos = Vector2.Clamp(_camPos, Vector2.Zero, _worldBound - new Vector2(v.Width, v.Height));
+        }
+
     }
 }
